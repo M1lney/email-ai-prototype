40076f36417c1d7047c09368df85356259fe1b9e
M1lney <tomj.milne@gmail.com>
2024-12-10 17:21:12 +1300

    Create card component to display brands

---
+++ app/ui/brands/brand-card.tsx
@@ -0,0 +1,23 @@
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+interface BrandCardProps {
+    website: string;
+    name: string;
+    onEdit: () => void;
+}
+
+export default function BrandCard({ website, name, onEdit,}: BrandCardProps) {
+    return (
+        <div className="flex flex-col space-y-2 rounded-lg border border-gray-200 bg-white p-4 shadow-sm">
+            <Card>
+                <Card.Header>{name}</Card.Header>
+                <Card.Body>
+                    <Card.Title>Special title treatment</Card.Title>
+                    <Card.Text>
+                        {website}
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+        </div>
+    );
+}
